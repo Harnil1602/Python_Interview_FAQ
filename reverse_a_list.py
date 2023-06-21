5c9336d030be9ebebc9d371250d1419fed376d36
harnil <harnilshah1602@gmail.com>
2023-06-22 02:04:53 +0530

    reversing a list using python

---
+++ reverse_a_list.py
@@ -0,0 +1,11 @@
+mylist1=[10,23,45,56,67,89]
+print("List before reversing",mylist1)
+
+mylist2=mylist1[::-1] # Slicing technique
+print("List after reversing",mylist2)
+
+
+'''mylist=[10,23,45,56,67,89]
+print("List before reversing",mylist)
+mylist.reverse()
+print("List after reversing",mylist)'''
